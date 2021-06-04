2abf4d9a7d36bde83fbe36c985c65ca5ecad14f6
angelmeryperez <57197822+angelmeryperez@users.noreply.github.com>
2021-06-04 18:19:19 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Toda_Las_posibilidades
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(377, 426);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView2.Location = new System.Drawing.Point(395, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(343, 426);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "X1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "X2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "X3";
+            this.Column3.Name = "Column3";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.dataGridView3.Location = new System.Drawing.Point(744, 12);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(242, 435);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Y!";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Y2";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(992, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 54);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "FILTRO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(992, 393);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 54);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(992, 72);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(67, 54);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Sumador";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(992, 132);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(67, 54);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Conteo";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1071, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;// para conectarse a bases de datos
+using System.Windows.Forms;
+
+namespace Toda_Las_posibilidades
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string data0 = "";
+        string data1 = "";
+
+        int W = 0;
+        int Num0 = 0;
+        int Num1 = 0;
+        int Num2 = 0;
+        int Num3 = 0;
+        int Num4 = 0;
+        int Num5 = 0;
+
+        double Num6 = 0;
+        double Sumador = 0;
+
+        int X1, X2, X3, X4 = 0;
+        bool vs = true;
+        bool cs = true;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            vs = true;
+            X3 = 0;
+            while (vs)
+            {
+                data0 = dataGridView1.Rows[X1].Cells[0].Value.ToString();
+
+                data1 = dataGridView1.Rows[X1].Cells[1].Value.ToString();
+                Num1 = int.Parse(data1);
+
+                while (vs)
+                {
+                    data0 = dataGridView1.Rows[X2].Cells[0].Value.ToString();
+                    Num2 = int.Parse(data0);
+                    data1 = dataGridView1.Rows[X2].Cells[1].Value.ToString();
+                    Num3 = int.Parse(data1);
+
+                    if ((Num0 == Num2) && (Num1 == Num3))
+                    {
+                        X3++;
+                    }
+
+                    if ((Num1 == Num2) && (Num0 == Num3))
+                    {
+                        X3++;
+                    }
+
+                    X2++;
+                    if (data0 == "100") { vs = false; }
+                }
+                vs = true;
+                X1++;
+                X2 = 0;
+                if (data0 == "100") { vs = false; }
+                dataGridView2.Rows[0].Cells[2].Value = X3;
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while (X1 <= 9999)
+            {
+                data0 = dataGridView2.Rows[X1].Cells[0].Value.ToString();
+                Num0 = int.Parse(data0);
+                data1 = dataGridView2.Rows[X1].Cells[1].Value.ToString();
+                Num1 = int.Parse(data1);
+                while (vs)
+                {
+                    data0 = dataGridView1.Rows[X2].Cells[1].Value.ToString();
+                    Num2 = int.Parse(data0);
+                    data1 = dataGridView1.Rows[X2].Cells[2].Value.ToString();
+                    Num3 = int.Parse(data1);
+                    if ((Num0 == Num2) && (Num1 == Num3))
+                    {
+                        dataGridView2.Rows[X1].Cells[2].Value = "X";
+                        vs = false;
+                    }
+                    else { dataGridView2.Rows[X1].Cells[2].Value = "Y"; }
+
+                    if ((Num1 == Num2) && (Num0 == Num3))
+                    {
+                        dataGridView2.Rows[X1].Cells[2].Value = "X";
+                        vs = false;
+                    }
+                    else { dataGridView2.Rows[X1].Cells[2].Value = "Y"; }
+
+                    X2++;
+                    if (data0 == "100") { vs = false; }
+                }
+                vs = true;
+                X1++;
+                X2 = 0;
+            }
+
+            //X1 = 0;
+            //while (X1 < 9999)
+            //{
+            //    data0 = dataGridView2.Rows[X1].Cells[2].Value.ToString();
+            //    if (data0 != "x")
+            //    {
+            //        dataGridView3.Rows.Add();
+            //        data0 = dataGridView2.Rows[X2].Cells[0].Value.ToString();
+            //        data1 = dataGridView2.Rows[X2].Cells[1].Value.ToString();
+            //        dataGridView3.Rows[X1].Cells[0].Value = data0;
+            //        dataGridView3.Rows[X1].Cells[1].Value = data1;
+            //        X2++;
+            //    }
+            //    X1++;
+            //}
+            //X1 = 0;
+        }
+        DataView ImportarDatos(string nombrearchivo) //COMO PARAMETROS OBTENEMOS EL NOMBRE DEL ARCHIVO A IMPORTAR
+        {
+
+            //UTILIZAMOS 12.0 DEPENDIENDO DE LA VERSION DEL EXCEL, EN CASO DE QUE LA VERSIÓN QUE TIENES ES INFERIOR AL DEL 2013, CAMBIAR A EXCEL 8.0 Y EN VEZ DE
+            //ACE.OLEDB.12.0 UTILIZAR LO SIGUIENTE (Jet.Oledb.4.0)
+            string conexion = string.Format("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = {0}; Extended Properties = 'Excel 12.0;'", nombrearchivo);
+            nombrearchivo = "Regristro Actualizado";
+            OleDbConnection conector = new OleDbConnection(conexion);
+
+            conector.Open();
+
+            //DEPENDIENDO DEL NOMBRE QUE TIENE LA PESTAÑA EN TU ARCHIVO EXCEL COLOCAR DENTRO DE LOS []
+            OleDbCommand consulta = new OleDbCommand("select * from [Hoja1$]", conector);
+
+            OleDbDataAdapter adaptador = new OleDbDataAdapter
+            {
+                SelectCommand = consulta
+            };
+
+            DataSet ds = new DataSet();
+
+            adaptador.Fill(ds);
+
+            conector.Close();
+
+            return ds.Tables[0].DefaultView;
+
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Sumador1();
+            dataGridView3.Rows[0].Cells[0].Value = Sumador;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            X1 = 0;
+            cs = true;
+            while (cs)
+            {
+                data0 = dataGridView1.Rows[X1].Cells[3].Value.ToString();
+                if (data0 == "100") { cs = false; break; }
+                if (data0 == "X") { X2++; } else { X3++; }
+                X1++;
+
+            }
+            dataGridView3.Rows[1].Cells[0].Value = X2 + "X";
+            dataGridView3.Rows[2].Cells[0].Value = X3 + "Y";
+            X1 = 0;
+            X2 = 0;
+            X3 = 0;
+            cs = true;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                //DE ESTA MANERA FILTRAMOS TODOS LOS ARCHIVOS EXCEL EN EL NAVEGADOR DE ARCHIVOS
+                Filter = "Excel | *.xls;*.xlsx;",
+
+                //AQUÍ INDICAMOS QUE NOMBRE TENDRÁ EL NAVEGADOR DE ARCHIVOS COMO TITULO
+                Title = "Seleccionar Archivo"
+            };
+            //EN CASO DE SELECCIONAR EL ARCHIVO, ENTONCES PROCEDEMOS A ABRIR EL ARCHIVO CORRESPONDIENTE
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                dataGridView1.DataSource = ImportarDatos(openFileDialog.FileName);
+            }
+            dataGridView1.Columns.Add("Column10", "X4");
+            dataGridView1.Columns.Add("Column11", "Numeros");
+            dataGridView2.Rows.Add(9999);
+            dataGridView3.Rows.Add(4);
+            for (int i = 0, k = 0; i < 100; i++)
+            {
+                for (int f = 0; f < 100; f++, k++)
+                {
+                    dataGridView2.Rows[k].Cells[1].Value = f;
+                    dataGridView2.Rows[k].Cells[0].Value = i;
+                }
+            }
+            X1 = 0;
+            while (true)
+            {
+                data0 = dataGridView1.Rows[X1].Cells[0].Value.ToString();
+                dataGridView1.Rows[X1].Cells[4].Value = X1;
+                X1++;
+                if (data0 == "100") 
+                {
+                    dataGridView1.Rows[X1].Cells[4].Value = X1;
+                    cs = false; break;          
+                }
+            }
+            X1 = 0;
+        }
+        void Sumador1(int colum = 0)
+        {
+            data0 = "0";
+            while (cs)
+            {
+                data0 = dataGridView1.Rows[X1].Cells[0].Value.ToString();
+                Num0 = int.Parse(data0);
+                if (Num0 == 100)
+                {
+                    X1 = 0;
+                    Validacion();
+                    if (X4 > 0)
+                    {
+                        Sumador1();
+                    }
+                    return;
+                }
+                Num1 = Num0 - 30;// Limite inferior de entrada
+                if (Num1 < 0) { Num1 = 0; }
+                Num2 = Num0 + 30;// Limite superior de entrada
+                if (Num2 > 99) { Num2 = 99; }
+                X1++;
+
+                data0 = dataGridView1.Rows[X1].Cells[0].Value.ToString();
+                Num3 = int.Parse(data0);
+
+                if ((Num3 >= Num1) && (Num3 <= Num2))// Si esta dentro del rango establecido
+                {
+                    dataGridView1.Rows[X1 - 1].Cells[3].Value = "X";
+                    while (cs)
+                    {
+                        Num4 = Num3 - 5;// Limite inferior de salida
+                        Num5 = Num3 + 5;// Limite superior de salida
+                        Num6 = Sumador + Num0;
+                            if (Num6 <= Num4) { Sumador += 0.5; }
+                            if (Num6 >= Num5) { Sumador -= 0.5; }
+                            if ((Num6 >= Num4) && (Num6 <= Num5)) { cs = false; } else { cs = true; }// Si cumple con los limites, sale del bucle
+                        dataGridView3.Rows[0].Cells[1].Value = X4;
+                    }
+                }
+                else { dataGridView1.Rows[X1 - 1].Cells[3].Value = "Y"; }
+                cs = true;
+                dataGridView1.Rows[X1 + 1].Cells[3].Value = "100";
+            }
+        }
+        void Validacion()
+        {
+            X2 = 0;
+            cs = true;
+            while (cs)
+            {
+                data0 = dataGridView1.Rows[X2].Cells[0].Value.ToString();
+                Num0 = int.Parse(data0);
+                if (Num0 == 100)
+                {
+                    cs = false;
+                    break;
+                }
+                Num1 = Num0 - 30;// Limite inferior de entrada
+                Num2 = Num0 + 30;// Limite superior de entrada
+                X2++;
+                data0 = dataGridView1.Rows[X2].Cells[0].Value.ToString();
+                Num3 = int.Parse(data0);
+                if ((Num3 >= Num1) && (Num3 <= Num2))// Si esta dentro del rango establecido
+                {
+                    Num4 = Num3 - 5;// Limite inferior de salida
+                    Num5 = Num3 + 5;// Limite superior de salida
+                    Num6 = Sumador + Num3;
+                    if ((Num6 >= Num4) && (Num6 <= Num5)) { } else { X4++; }
+                }
+            }
+        }
+    }
+}
